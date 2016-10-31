b61810cb79be9bd4881160371144d5ba02220857
eli <eli@vultr>
2016-10-30 18:15:39 -0700

    hmmm

--- sys/src/cmd/gs/386.h
+++ sys/src/cmd/gs/386.h
@@ -1,3 +1,12 @@
+/* 
+ * This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
 /* Parameters derived from machine and compiler architecture. */
 /* This file is generated mechanically by genarch.c. */
 
@@ -33,7 +42,7 @@
 
 	 /* ---------------- Cache sizes ---------------- */
 
-#define ARCH_CACHE1_SIZE 4194304
+#define ARCH_CACHE1_SIZE 131072
 #define ARCH_CACHE2_SIZE 4194304
 
 	 /* ---------------- Miscellaneous ---------------- */
