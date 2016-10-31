ed8578c06940444b3800c2962f709a2e5574de5a
echoline <401497+echoline@users.noreply.github.com>
2016-10-30 18:27:41 -0700

    moar_amd64

--- amd64/include/u.h
+++ amd64/include/u.h
@@ -1,20 +1,12 @@
-/* 
- * This file is part of the UCB release of Plan 9. It is subject to the license
- * terms in the LICENSE file found in the top-level directory of this
- * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
- * part of the UCB release of Plan 9, including this file, may be copied,
- * modified, propagated, or distributed except according to the terms contained
- * in the LICENSE file.
- */
-
 #define nil		((void*)0)
 typedef	unsigned short	ushort;
 typedef	unsigned char	uchar;
 typedef unsigned long	ulong;
 typedef unsigned int	uint;
-typedef   signed char	schar;
+typedef signed char	schar;
 typedef	long long	vlong;
 typedef	unsigned long long uvlong;
+typedef long long	intptr;
 typedef unsigned long long uintptr;
 typedef unsigned long	usize;
 typedef	uint		Rune;
